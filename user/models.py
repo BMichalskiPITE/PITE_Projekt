37e51016eac9f751115de9ed19c92bb0bdd80373
Wojciech Majkut <majkutw@gmail.com>
2018-05-27 18:22:32 +0200

    new fields in user model added

--- user/models.py
+++ user/models.py
@@ -3,11 +3,14 @@ from rest_framework.reverse import reverse
 # Create your models here.
 
 class User(models.Model):
-    user_id = models.CharField(max_length=200,primary_key=True)
+    id = models.CharField(max_length=200,primary_key=True)
+    username = models.CharField(max_length =200, default = "None")
+    mail = models.CharField(max_length = 200, default = "None")
+    imageUrl = models.CharField(max_length = 500, default = "None")
     is_guide = models.BooleanField(default=False)
 
     def __str__(self):
-        return str(self.user_id)
+        return str(self.id)
 
     def get_api_url(self, request = None):
-        return reverse("users-rud", kwargs ={'user_id': self.user_id}, request = request)
+        return reverse("users-rud", kwargs ={'id': self.id}, request = request)
