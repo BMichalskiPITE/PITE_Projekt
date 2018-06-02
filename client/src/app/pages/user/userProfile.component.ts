8159a828bf9f788e7208cb6d985eb898e48199ec
Magdalena Piłat <magdapilat123@gmail.com>
2018-06-02 15:22:25 +0200

    rating system

--- client/src/app/pages/user/userProfile.component.ts
+++ client/src/app/pages/user/userProfile.component.ts
@@ -42,10 +42,8 @@ export class UserProfileComponent implements OnInit{
   }
 
 
-starList: boolean[] = [true,true,true,true,true];       // create a list which contains status of 5 stars
+starList: boolean[] = [true,true,true,true,true]; 
 rating:number;
-//Create a function which receives the value counting of stars click, 
-//and according to that value we do change the value of that star in list.
 setStar(data:any){
       this.rating=data+1;                               
       for(let i=0;i<=4;i++){  
