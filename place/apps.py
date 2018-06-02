0354d93190595d5aefbc95cca2643eca677a608f
Mikolaj Szczepanski <mikolaj.szczepanski@outlook.com>
2018-06-02 15:50:39 +0200

    refactor sync places

--- place/apps.py
+++ place/apps.py
@@ -3,3 +3,9 @@ from django.apps import AppConfig
 
 class PlaceConfig(AppConfig):
     name = 'place'
+    google_maps_api_key = "AIzaSyDXJPkrTmAaD6AhH_7vFHBYxOEB1KZdqWo"
+    google_maps_sync_location = "50.062533,19.93732"
+    google_maps_sync_radius = "15000"
+    google_maps_sync_type = "museum"
+    google_maps_sync_max_pages = 5
+    google_maps_interval_sec = 5
